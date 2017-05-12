e5f3a2f2f687555ab19710e6f96b89ece1f769af
tianming <keyed.cn@gmail.com>
2017-05-12 18:04:12 +0800

    gengxin gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,61 @@
 'use strict';
 var gulp = require('gulp'),
     del = require('del'),
+    gulpLoadPlugins  =  require('gulp-load-plugins'),
+    del= require('del'),
+    stream = require('wiredep'),
     fileinclude = require('gulp-file-include'),
     browserSync = require('browser-sync').create();
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+const styles = {
+    'in': 'assets/stylesheets/**/*.scss',
+    'tmp': '.tmp/css'
+};
+const scripts = {
+    'in': 'assets/javascripts/**/*.js',
+    'tmp': '.tmp/js',
+    'out': 'dist/js'
+};
+const lint = {
+    'in': 'assets/javascripts/**/*.js'
+};
+const html = {
+    'in': 'assets/*.html',
+    'out': 'dist'
+};
+const images = {
+    'in': 'assets/images/**/*',
+    'out': 'dist/images'
+};
+const fonts = {
+    'in': ['assets/fonts/bootstrap/*'],
+    'tmp': '.tmp/fonts',
+    'out': 'dist/fonts'
+};
+const extras = {
+    'in': [
+        'assets/*.*',
+        '!assets/*.html'
+    ],
+    'out': 'dist'
+};
+const serve = {
+    'baseDir': ['.tmp', 'assets'],
+    'baseDirDist': ['dist'],
+    'routes': {
+        '/bower_components': 'bower_components'
+    },
+    'port': 9000
+};
+const build = {
+    'in': 'dist/**/*'
+};
+const wire = {
+    'in': 'assets/*.html',
+    'out': 'dist'
+};
 
 // 清除 dist 文件夹
 gulp.task('clean', function () {
@@ -13,7 +66,8 @@ gulp.task('clean', function () {
 gulp.task('html', function () {
     return gulp.src('./app/src/template/*.html')
     .pipe(fileinclude())
-    .pipe(gulp.dest('./app/dist'));
+    .pipe(gulp.dest('./app/dist'))
+    .pipe(reload({stream: true}));;
 });
 
 // 配置服务器
